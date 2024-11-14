b31db18221b0c5d104abf8a2045ede5f79321004
stepgo <167748477+stepg0@users.noreply.github.com>
2024-11-14 09:56:03 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,33 @@ provider "azurerm" {
   features {}
 }
 
+#Get resource group
+data "azurerm_resource_group" "wsdevops" {
+  name = var.rg_name
+}
+
+resource "azurerm_app_service_plan" "sp1" {
+  name                = var.app_service_plan_name
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "website" {
+  name                = var.web_app_name
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  app_service_plan_id = azurerm_app_service_plan.sp1.id
+
+  site_config {
+    linux_fx_version = "NODE|22-lts"
+    scm_type         = "LocalGit"
+  }
+}
+
